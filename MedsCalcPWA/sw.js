2eff713736915938e15217738743ca20b766f190
jaeames <jamesaeames@gmail.com>
2025-09-10 22:33:25 +0100

    Add files via upload

---
+++ MedsCalcPWA/sw.js
@@ -0,0 +1,30 @@
+// Very small offline cache. You can expand this later.
+const CACHE = "meds-calc-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) =>
+      cached || fetch(event.request).catch(() => caches.match("./index.html"))
+    )
+  );
+});
