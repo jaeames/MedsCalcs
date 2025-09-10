4916258ef2201848bc4ac66a5efe7414463b51bc
jaeames <jamesaeames@gmail.com>
2025-09-10 23:52:23 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Very small offline cache. You can expand this later.
-const CACHE = "meds-calc-v1";
+const CACHE = "meds-calc-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -32,4 +32,5 @@ self.addEventListener("fetch", (event) => {
     )
   );
 });
+
 
